eadab984ce8f28b6414515dc71ca1498c9a99d03
MD-REHMAN <Rehman.2468@gmail.com>
2018-10-23 13:14:54 +0530

    set up config.php

---
+++ api/config.php
@@ -0,0 +1,8 @@
+<?php
+        $servername = 'localhost';
+        $dbname = 'hrms';
+        $username = 'root';
+        $password = '123456';
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+?>
